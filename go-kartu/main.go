982d6205f3614186c8ded8700e3b340d219d136c
Naufal Afif <naufalafif58@gmail.com>
2019-07-26 17:12:07 +0700

    add some notes

--- go-kartu/main.go
+++ go-kartu/main.go
@@ -1,6 +1,12 @@
 package main
 
 func main() {
+	// dekBaru := dekbaru() // Membuat Dek Baru
+	// dekBaru.cetak() // Cetak Kartu
+	// dekBaru.acak() // Acak Kartu Dalam Dek
+	// dekBaru.simpanKeFile("__filedekku") // Simpan Dek Ke bentuk File
+	// dekDariFile := dakBaruDariFile("__filedekku") // Baca Dek Dari File
+	// kartuku := bagiKartu(dekBaru, 5) // Mengambil 5 Kartu
 
 	dekDariFile := dakBaruDariFile("my_cards")
 	dekDariFile.acak()
