63732312c0aefe1673d9e9bcb59fae21a5eb8a68
Naufal Afif <naufalafif58@gmail.com>
2019-07-26 18:04:02 +0700

    fix wrong function name

--- go-kartu/main.go
+++ go-kartu/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	// dekBaru := dekbaru() // Membuat Dek Baru
 	// dekBaru.cetak() // Cetak Kartu
-	// dekBaru.acak() // Acak Kartu Dalam Dek
+	// dekTerAcak := acak(dekBaru) // Acak Kartu Dalam Dek
 	// dekBaru.simpanKeFile("__filedekku") // Simpan Dek Ke bentuk File
 	// dekDariFile := dakBaruDariFile("__filedekku") // Baca Dek Dari File
 	// kartuku := bagiKartu(dekBaru, 5) // Mengambil 5 Kartu
