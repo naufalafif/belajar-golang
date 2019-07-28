2c443a169aee62d46efa4f3b23c6cfbf003fec35
Naufal Afif <naufalafif58@gmail.com>
2019-07-28 18:16:06 +0700

    add different way to declare map

--- dasar/map/map.go
+++ dasar/map/map.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 
+	// Cara Deklaasi map ke 1 // deklarasi ekspresi memerlukan make
 	contohMap := make(map[string]string)
 	contohMap["nama"] = "naufal"
 	contohMap["alamat"] = "jogja"
@@ -11,4 +12,17 @@ func main() {
 	delete(contohMap, "alamat")
 
 	fmt.Println(contohMap)
+
+	// Cara Deklarasi Map ke 2
+	warna := map[string]string{
+		"merah": "f21312312",
+		"hijau": "f5435345",
+	}
+
+	fmt.Println(warna)
+
+	// Cara Deklarasi Map ke 3
+	var satuan map[string]string
+
+	fmt.Println(satuan)
 }
