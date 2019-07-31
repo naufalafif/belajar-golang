bea7987ea84ba6b9ba594a813522fdc7455225e1
Naufal Afif <naufalafif58@gmail.com>
2019-07-31 13:03:15 +0700

    add user defined writter

--- go-http/main.go
+++ go-http/main.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+// Writter Buatan sendiri
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("https://www.google.com")
 
@@ -15,14 +18,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Cara Manual
-	// -- Membuat Variable untuk menampung byte dari response body
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)      // mengisi data bs dengan response body
-	fmt.Println(string(bs)) // mengubah byte ke string
+	// // Cara Manual
+	// // -- Membuat Variable untuk menampung byte dari response body
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)      // mengisi data bs dengan response body
+	// fmt.Println(string(bs)) // mengubah byte ke string
 
-	// Cara ke 2
-	// -- fungsi io.copy digunakan untuk untuk menuliskan/menampilkan data berupa reader ke output yang di inginkan
-	// kode dibawah menggunakan os.stdout/terminal sebagai output/writter dan resp.body sebagai output
-	io.Copy(os.Stdout, resp.Body)
+	// // Cara ke 2
+	// // -- fungsi io.copy digunakan untuk untuk menuliskan/menampilkan data berupa reader ke output yang di inginkan
+	// // kode dibawah menggunakan os.stdout/terminal sebagai output/writter dan resp.body sebagai output
+	// io.Copy(os.Stdout, resp.Body)
+
+	// Cara ke 3
+	// Menggunakan Writer - buatan
+	lg := logWriter{}
+	io.Copy(lg, resp.Body)
+}
+
+func (logWriter) Write(p []byte) (n int, err error) {
+	fmt.Println(string(p))
+
+	return len(p), nil
 }
