31fb9b90d8e1a6938db0f888131cd372e97f5f9c
Naufal Afif <naufalafif58@gmail.com>
2019-08-01 18:16:20 +0700

    init channel

--- lanjut/channel/main.go
+++ lanjut/channel/main.go
@@ -13,18 +13,39 @@ func main() {
 		"https://golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, url := range daftarWebsite {
-		go cekLink(url)
+		go cekLink(url, c)
 	}
 
-	fmt.Scanln()
+	// Cara 1 menampilkan hasil channel. print dilakukan 5 kali, karena main routine akan menutup program jika semua line telah tereksekusi
+	// fmt.Println(<-c) // Bloking Call
+	// fmt.Println(<-c) // Bloking Call
+	// fmt.Println(<-c) // Bloking Call
+	// fmt.Println(<-c) // Bloking Call
+	// fmt.Println(<-c) // Bloking Call
+
+	// Cara 2
+	// Cara yang sama dengan cara ke 1, tetapi menggunakan iterasi
+	// for i := 1; i <= len(daftarWebsite); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// Cara ke 3
+	for l := range c {
+		fmt.Println(l)
+	}
+
 }
 
-func cekLink(url string) {
+func cekLink(url string, c chan string) {
 	_, err := http.Get(url)
 	if err != nil {
 		fmt.Println(url, "sedang down")
+		c <- url
 		return
 	}
 	fmt.Println(url, "jalan normal")
+	c <- "jalan normal"
 }
