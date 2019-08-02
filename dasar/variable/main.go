6b6591589e93abcd0f4a040a6d845276e881a8b0
Naufal Afif <naufalafif58@gmail.com>
2019-08-02 21:23:52 +0700

    init variable

---
+++ dasar/variable/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Cara deklarasi 1
+	var nama string = "budi"
+
+	// Cara deklarasi 2
+	tinggi := "2 meter"
+
+	// Deklarasi Ganda
+	var panjang, lebar int = 1, 2
+
+	// Deklarasi Ganda 2
+	var ukuranSepatu, ukuranPinggang int = 27, 32
+
+	fmt.Println(nama, tinggi)
+	fmt.Println(panjang, lebar)
+	fmt.Println(ukuranPinggang, ukuranSepatu)
+}
